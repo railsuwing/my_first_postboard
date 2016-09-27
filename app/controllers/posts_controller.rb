45a8a70b29e4af5a5a8253de25d1ca708a53b719
railsuwing <chenathome0810@seed.net.tw>
2016-09-28 01:26:37 +0800

    add comment table(MVC)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,10 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-   
- #  def show
- # 	@post = Post.find(params[:id])
- # end
+  
+  def show
+ 		@post = Post.find(params[:id])
+ 		@comment = Comment.new
+  	@comments = @post.comments
+  end
 end
