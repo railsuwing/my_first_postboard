aac0e89de46b319753ca2a5c1276e01174677739
railsuwing <chenathome0810@seed.net.tw>
2016-09-28 17:49:13 +0800

    added container css class

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,8 @@ class PostsController < ApplicationController
   end
   
   def show
+  	 #Rails 會把用戶所有傳進來的參數打包成一個叫 params 的 Hash
+    #find 代表搜尋某一比資料，而 params[:id] 代表用戶想查詢的資料 id
  		@post = Post.find(params[:id])
  		@comment = Comment.new
   	@comments = @post.comments
