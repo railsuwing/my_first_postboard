96744cecba49db68db9213941795a80abe42a2e2
railsuwing <chenathome0810@seed.net.tw>
2016-09-28 17:36:37 +0800

    make html good look a little bit

--- config/routes.rb
+++ config/routes.rb
@@ -56,5 +56,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  resources :posts
+resources :posts do 
+    resources :comments 
+  end
 end
