9d9053efd446aebee7655d6faa5d7338a7f39ac3
railsuwing <chenathome0810@seed.net.tw>
2016-09-29 23:58:24 +0800

    added login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+       flash[:notice]= "You have registered"
       redirect_to root_path
     else
       render :new
